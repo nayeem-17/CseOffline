468622ebaf5de99d029aef0bafa5068c86a22f9a
nayeem-17 <nasa.28apr@gmail.com>
2021-08-30 12:11:42 +0600

    quicksort fixed

--- level2_term1/DSA/offline07/java/QuickSort.java
+++ level2_term1/DSA/offline07/java/QuickSort.java
@@ -1,14 +1,14 @@
 package level2_term1.DSA.offline07.java;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
     public static void sort(int[] Arr, int low, int high) {
         if (low < high) {
             int q = partition(Arr, low, high);
-            if (q > low)
-                sort(Arr, low, q - 1);
-            if (q < high)
-                sort(Arr, q + 1, high);
+            sort(Arr, low, q - 1);
+            sort(Arr, q + 1, high);
         }
     }
 
@@ -43,20 +43,9 @@ public class QuickSort {
         return j;
     }
 
-    // public static void main(String[] args) {
-    // int[] arr = new int[1000000];
-    // for (int i = 0; i < arr.length; i++)
-    // arr[i] = (int) (Math.random() * 10000) * (int) (Math.random() * 100000) +
-    // (int) (Math.random() * 100000);
-    // // int arr[] = { 59, 20, 62, 41, 32, 60, 66, 30, 96, 83 };
-    // // System.out.println(Arrays.toString(arr));
-    // // System.out.println(partition(arr, 0 + 8, arr.length - 1));
-    // // Arrays.sort(arr, Collections.reverseOrder());
-    // int[] arrDesc =
-    // Arrays.stream(arr).boxed().sorted().mapToInt(Integer::intValue).toArray();
-    // double startTime2 = System.nanoTime();
-    // QuickSort.sort(arrDesc, 0, arrDesc.length - 1);
-    // double endTime2 = System.nanoTime();
-    // System.out.println("Time : " + (endTime2 - startTime2) / 1000000000);
-    // }
+    public static void main(String[] args) {
+        int arr[] = { 59, 20, 62, 41, 32, 60, 66, 30, 96, 83 };
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
 }
