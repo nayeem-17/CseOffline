925962f4b7decd464a764eb1350444b3f3372821
nayeem-17 <nasa.28apr@gmail.com>
2021-12-01 07:53:12 +0600

    template added

--- level2_term2/DSA2/Template/Graph.java
+++ level2_term2/DSA2/Template/Graph.java
@@ -63,4 +63,93 @@ public class Graph {
         }
     }
 
+    public boolean isBipartite() {
+        int[] colors = new int[length];
+        int[] visited = new int[length];
+        int currentColor = 0;
+        visited[0] = 1;
+        colors[0] = currentColor;
+        return isBipartite(0, currentColor, visited, colors);
+    }
+
+    private boolean isBipartite(int start, int currentColor, int[] visited, int[] colors) {
+        visited[start] = 1;
+        int nextColor = (currentColor == 0) ? 1 : 0;
+        for (int i = 0; i < nodes.get(start).size(); i++) {
+            int next = nodes.get(start).get(i);
+            if (visited[next] == 0) {
+                if (isBipartite(next, nextColor, visited, colors) == false)
+                    return false;
+            } else if (colors[next] == currentColor)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean hasCycle() {
+        boolean[] visited = new boolean[length];
+        return hasCycle(0, visited, -1);
+    }
+
+    private boolean hasCycle(int start, boolean[] visited, int parent) {
+        visited[start] = true;
+        for (int i = 0; i < nodes.get(start).size(); i++) {
+            int next = nodes.get(start).get(i);
+            if (visited[next] == false) {
+                if (hasCycle(next, visited, parent))
+                    return true;
+            } else if (next != parent)
+                return true;
+
+        }
+        return false;
+    }
+
+    // topological sort
+    public List<Integer> topologicalSort() {
+        List<Integer> result = new ArrayList<Integer>();
+        boolean[] visited = new boolean[length];
+        for (int i = 0; i < length; i++) {
+            if (!visited[i]) {
+                topologicalSort(i, visited, result);
+            }
+        }
+        return result;
+    }
+
+    private void topologicalSort(int i, boolean[] visited, List<Integer> result) {
+        visited[i] = true;
+        for (int j = 0; j < nodes.get(i).size(); j++) {
+            int next = nodes.get(i).get(j);
+            if (!visited[next]) {
+                topologicalSort(next, visited, result);
+            }
+        }
+        result.add(i);
+    }
+
+    // strongly connected components
+    public List<List<Integer>> stronglyConnectedComponents() {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int[] visited = new int[length];
+        for (int i = 0; i < length; i++) {
+            if (visited[i] == 0) {
+                List<Integer> component = new ArrayList<Integer>();
+                stronglyConnectedComponents(i, visited, component);
+                result.add(component);
+            }
+        }
+        return result;
+    }
+
+    private void stronglyConnectedComponents(int i, int[] visited, List<Integer> component) {
+        visited[i] = 1;
+        component.add(i);
+        for (int j = 0; j < nodes.get(i).size(); j++) {
+            int next = nodes.get(i).get(j);
+            if (visited[next] == 0) {
+                stronglyConnectedComponents(next, visited, component);
+            }
+        }
+    }
 }
